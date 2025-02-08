46a0d6d37e0c52a11ed3996bc5de79038544082d
Keison28 <felucho.cerda@gmail.com>
2025-02-07 23:08:18 -0400

    CRUD creado
    Finalizacion del CRUD

---
+++ Tarea 2/Controllers/libroesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tarea_2.Data;
+using Tarea_2.Models;
+
+namespace Tarea_2.Controllers
+{
+    public class libroesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public libroesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.libros.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.libros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: libroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+        }
+
+        // GET: libroes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.libros.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: libroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,  libro libro)
+        {
+            if (id != libro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!libroExists(libro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+        }
+
+        // GET: libroes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.libros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        // POST: libroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libro = await _context.libros.FindAsync(id);
+            if (libro != null)
+            {
+                _context.libros.Remove(libro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool libroExists(int id)
+        {
+            return _context.libros.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Tarea 2/Models/libro.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea_2.Models
+{
+    public class libro
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60)]
+        public string Titulo { get; set; }
+        [StringLength(60)]
+
+        public string Autor { get; set; }
+        [StringLength(40)]
+
+        public string Genero { get; set; }
+        [StringLength(40)]
+
+        public string Disponibilidad { get; set; }
+
+    }
+}
